4a897dd78db4b55c7c1ca992eac9a0cd90d40dd4
sonnymay <thesan_555@hotmail.com>
2023-05-08 11:32:27 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,39 +1,50 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-API_KEY = "c8fbc8a4da6f4d2376db9c4c7e5d3c99"
+load_dotenv()
 
-def get_weather_data(city):
-    url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric&cnt=4"
+API_KEY = os.environ.get('OPENWEATHER_API_KEY')
+
+def get_weather_data(location):
+    # Check if the location is a digit, if so assume it's a zip code
+    if location.isdigit():
+        url = f"https://api.openweathermap.org/data/2.5/weather?zip={location},us&appid={API_KEY}&units=metric"
+    else:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}&units=metric"
+        
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         return response.json()
     else:
         print(f"Error fetching weather data: {response.status_code}")
         return None
 
+def display_weather_data(weather_data):
+    city = f"{weather_data['name']}, {weather_data['sys']['country']}"
+    temp_celsius = weather_data['main']['temp']
+    temp_fahrenheit = (temp_celsius * 1.8) + 32
+    feels_like_celsius = weather_data['main']['feels_like']
+    feels_like_fahrenheit = (feels_like_celsius * 1.8) + 32
+    description = weather_data['weather'][0]['description']
 
-def display_weather_data(city, weather_data):
-    if weather_data is not None:
-        print(f"Weather forecast for {city}:\n")
-        
-        for entry in weather_data["list"]:
-            dt_txt = entry["dt_txt"]
-            temp = entry["main"]["temp"]
-            description = entry["weather"][0]["description"]
-            
-            print(f"{dt_txt}: {temp:.1f}°C, {description}")
-
+    print(f"\nWeather forecast for {city}:")
+    print(f"Temperature: {temp_celsius:.1f}°C ({temp_fahrenheit:.1f}°F)")
+    print(f"Feels like: {feels_like_celsius:.1f}°C ({feels_like_fahrenheit:.1f}°F)")
+    print(f"Description: {description}")
 
 def main():
     while True:
-        city = input("\nEnter the name of a city (or 'exit' to quit): ")
+        location = input("\nEnter a city name or zip code (or 'exit' to quit): ")
         
-        if city.lower() == "exit":
+        if location.lower() == "exit":
             break
         
-        weather_data = get_weather_data(city)
-        display_weather_data(city, weather_data)
+        weather_data = get_weather_data(location)
+        
+        if weather_data is not None:
+            display_weather_data(weather_data)
 
 
 if __name__ == "__main__":
